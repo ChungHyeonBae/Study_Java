36529b10a1504156653bea5b66bfaf769e4124c0
Chunghyeon Bae <144658490+ChungHyeonBae@users.noreply.github.com>
2023-09-18 17:48:42 +0900

    Create Test02_If.java

---
+++ WorkSpace/04.ControlStatement/src/pack01/exif/Test02_If.java
@@ -0,0 +1,13 @@
+package pack01.exif;
+
+public class Test02_If {
+	public static void main(String[] args) {
+		int num = 21;
+			if (num % 2 != 0) {//==
+				System.out.println("num은 짝수인가");
+			} else {
+				System.out.println("num은 홀수이다.");
+
+			}
+	}
+}

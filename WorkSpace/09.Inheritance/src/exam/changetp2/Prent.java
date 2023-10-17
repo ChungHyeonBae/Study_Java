e3762373e3152da806a905ea5a97ca5bb0a4e183
Chunghyeon Bae <144658490+ChungHyeonBae@users.noreply.github.com>
2023-10-17 17:38:59 +0900

    20231017
    수업

---
+++ WorkSpace/09.Inheritance/src/exam/changetp2/Prent.java
@@ -0,0 +1,13 @@
+package exam.changetp2;
+
+public class Prent {
+	public void method1() {
+		System.out.println();
+		
+	}
+
+	public void method2() {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- WorkSpace/98.ExamOOP/src/member/MemberDTO.java
+++ WorkSpace/98.ExamOOP/src/member/MemberDTO.java
@@ -1,72 +1,93 @@
 package member;
 
 public class MemberDTO {
-   private String id , pw, name , nickname;
-   private int money;
-   private boolean isAdmin; //데이터가 true면 관리자 else면 사용자 
-   
-   public String getId() {
-      return id;
-   }
+	private String id , pw , name , nickName;
+	private int money;
+	private boolean isAdmin;//<=데이터가 true이면 관리자 else 사용자
+	
+	
+	
+	public String getId() {
+		return id;
+	}
 
-   public void setId(String id) {
-      this.id = id;
-   }
 
-   public String getPw() {
-      return pw;
-   }
 
-   public void setPw(String pw) {
-      this.pw = pw;
-   }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-   public String getName() {
-      return name;
-   }
 
-   public void setName(String name) {
-      this.name = name;
-   }
 
-   public String getNickname() {
-      return nickname;
-   }
+	public String getPw() {
+		return pw;
+	}
 
-   public void setNickname(String nickname) {
-      this.nickname = nickname;
-   }
 
-   public int getMoney() {
-      return money;
-   }
 
-   public void setMoney(int money) {
-      this.money = money;
-   }
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
 
-   public boolean isAdmin() {
-      return isAdmin;
-   }
 
-//   public void setAdmin(boolean isAdmin) {
-//      this.isAdmin = isAdmin;
-//   }
 
-   public MemberDTO(String id, String pw, String name, String nickname, boolean isAdmin) {//관리자 데이터
-      super();
-      this.id = id;
-      this.pw = pw;
-      this.name = name;
-      this.nickname = nickname;
-      this.isAdmin = isAdmin;
-   }
+	public String getName() {
+		return name;
+	}
 
-   public MemberDTO(String id, String pw, String name, String nickname) {//사용자 데이터 
-      super();
-      this.id = id;
-      this.pw = pw;
-      this.name = name;
-      this.nickname = nickname;
-   }
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getNickName() {
+		return nickName;
+	}
+
+
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+
+
+	public int getMoney() {
+		return money;
+	}
+
+
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+
+//
+//	public void setAdmin(boolean isAdmin) {
+//		this.isAdmin = isAdmin;
+//	}
+	public MemberDTO(String id, String pw, String name, String nickName, boolean isAdmin) {
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.nickName = nickName;
+		this.isAdmin = isAdmin;
+	}
+	public MemberDTO(String id, String pw, String name, String nickName) {
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.nickName = nickName;
+	}
+	
 }

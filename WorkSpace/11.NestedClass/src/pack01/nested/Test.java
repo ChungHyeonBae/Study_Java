94a67b254d5931d555ff5a1965bba277ed7a75bd
Chunghyeon Bae <144658490+ChungHyeonBae@users.noreply.github.com>
2023-10-24 17:49:07 +0900

    20231024
    수업

---
+++ WorkSpace/11.NestedClass/src/pack01/nested/Test.java
@@ -0,0 +1,27 @@
+package pack01.nested;
+
+public class Test {
+	//정답포인트 , 10 이 각각 출력되면 정답
+		private static class TestA {
+			
+			int field = 10;
+			public void method1() {
+				
+				System.out.println("정답 포인트");
+			}
+			
+		}
+		
+
+
+		public static class TestC {
+			public class TestZ {
+				public class TestB {
+					public TestA rtnMethod() {
+						return new TestA();
+					
+				}
+			}
+		}
+	} 
+}
---
+++ WorkSpace/11.NestedClass/src/pack01/nested/TestMain.java
@@ -0,0 +1,10 @@
+package pack01.nested;
+
+
+
+public class TestMain {
+	public static void main(String[] args) {
+		
+		
+	}
+}
